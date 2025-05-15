38028c6cd1c7949f4c7670d256b9793c965750b9
RogerVinals <roger.vinals@estudiantat.upc.edu>
2025-05-15 18:12:05 +0200

    Esto ya funciona y se puede publicar

--- venv/maze.py
+++ venv/maze.py
@@ -1,35 +1,40 @@
 #Build base del maze game
 import sys
 
+import laberinto
 from personaje import Personaje
-from laberinto import laberintos
+from muros import Muro
 import pygame
+
 
 pygame.init()
 
-WIDTH = 600 #Ancho
-HEIGHT =650 #Alto
+WIDTH = 900 #Ancho
+HEIGHT = 650 #Alto
 screen = pygame.display.set_mode([WIDTH, HEIGHT]) #Display de la pantalla
 timer = pygame.time.Clock() #Timer para que se ejecute
 fps = 60
-font = pygame.font.Font('freesansbold.ttf', 20)
-level = laberintos
-pygame.display.set_caption('maze game')
+pygame.display.set_caption('MAZEGAME')
 gameIcon = pygame.image.load('../sprites/fotojuego.jpg')
 pygame.display.set_icon(gameIcon)
 
-#Posicion Inicial
-player = Personaje((0,255,0),5,5)
-player.rect.x = 100
-player.rect.y = 100
+#Sprites
+player = Personaje(50,50)
 all_sprites_list = pygame.sprite.Group()
 all_sprites_list.add(player)
+paredes = pygame.sprite.Group()
+
+for x, y, w, h in laberinto.paredes_lista_1: #Dibujo del laberinto seleccionado
+    pared = Muro(x, y, w, h)
+    paredes.add(pared)
+    all_sprites_list.add(pared)
+
 
 #Bucle del funcionamiento del juego
 run = True
 while run:
     timer.tick(fps)
-    screen.fill('black')
+    screen.fill('gray')
 
     #Event que va leyendo los eventos y hace un bucle de los eventos dentro de bucle del juego
     for event in pygame.event.get():
@@ -37,19 +42,10 @@ while run:
             run = False
             sys.exit()
 
-    #Movimiento, esta fuera del event ya que no podrias mantenerlos pulsados, el resto deberia ir en event
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        Personaje.move(player,pygame.K_LEFT)
-    if keys[pygame.K_RIGHT]:
-        Personaje.move(player,pygame.K_RIGHT)
-    if keys[pygame.K_UP]:
-        Personaje.move(player,pygame.K_UP)
-    if keys[pygame.K_DOWN]:
-        Personaje.move(player,pygame.K_DOWN)
+    player.move(paredes,WIDTH,HEIGHT) #Llama a la funcion Personaje.move
 
-    all_sprites_list.update()
-    all_sprites_list.draw(screen)
-    pygame.display.flip()
+    all_sprites_list.update() #Update la lista de sprites
+    all_sprites_list.draw(screen) #Dibuja Sprites
+    pygame.display.flip() #Reset pantalla por cada tick
 
 pygame.quit()
--- venv/personaje.py
+++ venv/personaje.py
@@ -3,23 +3,35 @@ from pygame import sprite, K_LEFT
 from pygame.sprite import Sprite
 
 
-class Personaje(pygame.sprite.Sprite): #clase jugador
-        def __init__(self,color,width,height):
-                super().__init__()
-                self.image = pygame.Surface([50,50])
-                self.image.fill((0,255,0))
-                pygame.draw.rect(self.image,color,pygame.Rect(0,0,50,50))
-                self.rect = self.image.get_rect()
 
-        def move(self,tecla):
-                if tecla == pygame.K_LEFT:
-                        self.rect.x -= 2
-                if tecla == pygame.K_RIGHT:
-                        self.rect.x += 2
-                if tecla == pygame.K_UP:
-                        self.rect.y -= 2
-                if tecla == pygame.K_DOWN:
-                        self.rect.y += 2
+class Personaje(pygame.sprite.Sprite): #clase jugador
+        def __init__(self,width,height):
+                super().__init__()
+                self.image = pygame.transform.scale(pygame.image.load("../sprites/pngwing.com.png"),(width,height)).convert_alpha()
+                self.rect = self.image.get_rect(topleft=(width,height))
+                self.velocidad = 5
+                self.rect.x = 0
+                self.rect.y = 0
+
+        def move(self, paredes,ancho,alto):
+                keys = pygame.key.get_pressed()
+                dx = dy = 0
+                if keys[pygame.K_LEFT]: dx = -self.velocidad
+                if keys[pygame.K_RIGHT]: dx = self.velocidad
+                if keys[pygame.K_UP]: dy = -self.velocidad
+                if keys[pygame.K_DOWN]: dy = self.velocidad
+
+                # Movimiento horizontal y colisiones
+                self.rect.x += dx
+                if pygame.sprite.spritecollideany(self, paredes) or self.rect.left < 0 or self.rect.right > ancho :
+                        self.rect.x -= dx
+
+                # Movimiento vertical y colisiones
+                self.rect.y += dy
+                if pygame.sprite.spritecollideany(self, paredes) or self.rect.bottom > alto or self.rect.top < 0 :
+                        self.rect.y -= dy
+
+
 
 
 
